c0b07f0048f8f7f75a08217b9bca52f4c45262ff
jessicarslawson <jessicarslawson@gmail.com>
2015-11-01 00:01:58 -0400

    adds additional styles and JS

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+$(".pic").mouseenter(function() {
+  $( this ).css("box-shadow", "0px 0px 10px 10px rgba(184,39,174,1");
+});
+
+
+$(".pic").mouseleave(function() {
+  $( this ).css("box-shadow" , "initial");
+});
+
